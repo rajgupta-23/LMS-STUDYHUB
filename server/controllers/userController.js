0bdf33046273f497b48ba216100ce9188ea8098f
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 23:06:33 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,13 +1,10 @@
 const User = require("../models/User");
-
 const Enrollment = require("../models/Enrollment");
 const Certificate = require("../models/Certificate");
 
 
 const dashboard = async (req, res) => {
-
     try {
-
         const enrollments = await Enrollment.find({
             student: req.user._id
         })
@@ -15,54 +12,35 @@ const dashboard = async (req, res) => {
                 "course",
                 "title thumbnail price"
             );
-
-
         const certificates = await Certificate.countDocuments({
             user: req.user._id
         });
-
-
-
         const completed = enrollments.filter(
             item => item.progress === 100
         ).length;
-
-
-
         res.json({
-
             user: {
                 name: req.user.name,
                 email: req.user.email,
                 role: req.user.role
             },
-
-
             stats: {
                 enrolled: enrollments.length,
                 completed,
                 certificates
             },
-
-
             courses: enrollments
-
         });
-
-
-
     }
     catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
 
 };
 
-// Get Profile
+
 const getProfile = async (req, res) => {
     try {
         const user = await User.findById(req.user._id)
@@ -80,25 +58,17 @@ const getProfile = async (req, res) => {
     }
 };
 
-
-
-// Update Profile
 const updateProfile = async (req, res) => {
     try {
-
         const { name, email, bio } = req.body;
-
         const updateData = {
             name,
             email,
             bio,
         };
-
-        // Avatar Upload
         if (req.file) {
             updateData.avatar = `/uploads/avatars/${req.file.filename}`;
         }
-
         const user = await User.findByIdAndUpdate(
             req.user._id,
             updateData,
