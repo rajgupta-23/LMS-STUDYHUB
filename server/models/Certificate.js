8c6067784f06d2a6fa4014b57987fc6cf4042a49
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 22:28:47 +0530

    Update Certificate.js

--- server/models/Certificate.js
+++ server/models/Certificate.js
@@ -1,36 +1,24 @@
 const mongoose = require("mongoose");
-
-
 const certificateSchema = new mongoose.Schema(
-    {
-
         user: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
             required: true,
         },
-
-
         course: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Course",
             required: true,
         },
-
-
         certificateId: {
             type: String,
             required: true,
             unique: true,
         },
-
-
         issueDate: {
             type: Date,
             default: Date.now,
         },
-
-
         status: {
             type: String,
             enum: [
@@ -39,34 +27,24 @@ const certificateSchema = new mongoose.Schema(
             ],
             default: "completed",
         },
-
-
         pdfUrl: {
             type: String,
             default: "",
         },
-
-
         verificationCode: {
             type: String,
             unique: true,
             required: true,
         },
-
-
         instructorName: {
             type: String,
             default: "",
         },
-
-
     },
     {
         timestamps: true,
     }
 );
-
-
 module.exports =
     mongoose.model(
         "Certificate",
