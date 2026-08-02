249a216c3c3a72c3e3532a2b915434736f0756bf
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 22:34:41 +0530

    Update Payment.js

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -1,107 +1,57 @@
 const mongoose = require("mongoose");
-
-
 const paymentSchema = new mongoose.Schema(
     {
-
         user: {
-
             type: mongoose.Schema.Types.ObjectId,
-
             ref: "User",
-
             required: true
-
         },
-
-
         course: {
-
             type: mongoose.Schema.Types.ObjectId,
-
             ref: "Course",
-
             required: true
-
         },
-
-
         amount: {
-
             type: Number,
-
             required: true
-
         },
-
-
         paymentId: {
-
             type: String,
             sparse: true
-
         },
-
-
         orderId: {
-
             type: String,
-
             required: true
-
         },
-
-
         currency: {
-
             type: String,
-
             default: "INR"
-
         },
-
-
         status: {
-
             type: String,
-
             enum: [
                 "pending",
                 "success",
                 "failed"
             ],
-
             default: "pending"
-
         },
-
-
         paymentMethod: {
-
             type: String
-
         },
         transactionDate: {
-
             type: Date,
-
             default: Date.now
-
         },
         receiptUrl: {
             type: String,
             default: ""
         },
     },
-
     {
         timestamps: true
     }
-
 );
-
-
-
 module.exports = mongoose.model(
     "Payment",
     paymentSchema
