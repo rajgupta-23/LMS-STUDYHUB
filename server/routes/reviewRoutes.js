507452c7a7f25187bc2ea084f8c89e06fa7ae54a
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 23:17:50 +0530

    Update reviewRoutes.js

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,27 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
 const {
     addReview,
     getReviews,
 } = require("../controllers/reviewController");
-
 const { protect } = require("../middleware/auth");
 
 
-// Add Review
-router.post(
-    "/",
-    protect,
-    addReview
-);
-
-
-// Get Course Reviews
-router.get(
-    "/:courseId",
-    getReviews
-);
-
-
+router.post("/",protect,addReview);
+router.get("/:courseId",getReviews);
 module.exports = router;
