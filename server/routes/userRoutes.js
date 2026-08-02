ddbdc09d206e012a814a4b8f08a1ee3a7136b8a8
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 23:18:56 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,16 +5,10 @@ const {
     getProfile,
     updateProfile,
 } = require("../controllers/userController");
-
 const { protect } = require("../middleware/auth");
 const { avatarUpload } = require("../middleware/upload");
-
 router.get("/dashboard", protect, dashboard);
-
-// Get Profile
 router.get("/profile", protect, getProfile);
-
-// Update Profile
 router.put("/profile", protect, avatarUpload.single("avatar"), updateProfile);
 
 module.exports = router;
