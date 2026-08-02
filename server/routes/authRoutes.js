ccc8e5a6aa279313b11d0afbccbb6e38c17fe9e8
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 23:11:08 +0530

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,62 +1,17 @@
 const express = require("express");
-
 const router = express.Router();
-
-
 const {
-
     register,
-
     login,
-
     sendResetOTP,
-
     resetPassword,
-
     me
-
-
 } = require("../controllers/authController");
-
-
 const { protect } = require("../middleware/auth");
 
-
-
-router.post(
-    "/register",
-    register
-);
-
-
-
-router.post(
-    "/login",
-    login
-);
-
-
-
-router.post(
-    "/forgot-password",
-    sendResetOTP
-);
-
-
-
-router.post(
-    "/reset-password",
-    resetPassword
-);
-
-
-
-router.get(
-    "/me",
-    protect,
-    me
-);
-
-
-
+router.post("/register",register);
+router.post("/login",login);
+router.post("/forgot-password",sendResetOTP);
+router.post("/reset-password",resetPassword);
+router.get("/me",protect,me);
 module.exports = router;
