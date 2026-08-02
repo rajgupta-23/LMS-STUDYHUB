93a22525fc3d6ad620b4f7be33d543a55c27078c
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 23:11:47 +0530

    Update categoryRoutes.js

--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,27 +1,16 @@
 const express = require("express");
 const router = express.Router();
-
 const Course = require("../models/Course");
 
-
 router.get("/", async (req, res) => {
-
     try {
-
         const categories = await Course.distinct("category");
-
         res.json(categories);
-
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
 });
-
 
 module.exports = router;
