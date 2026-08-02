518e819a31b42300640537bd52609158d4222ae3
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 23:18:29 +0530

    Update statsRoutes.js

--- server/routes/statsRoutes.js
+++ server/routes/statsRoutes.js
@@ -4,44 +4,27 @@ const router = express.Router();
 const User = require("../models/User");
 const Course = require("../models/Course");
 
-
-
 router.get("/", async (req, res) => {
-
     try {
-
         const students = await User.countDocuments({
             role: "student"
         });
-
-
         const instructors = await User.countDocuments({
             role: "instructor"
         });
-
-
         const courses = await Course.countDocuments();
 
-
-
         res.json({
-
             students,
             instructors,
             courses,
             rating: 4.8
-
         });
-
-
     } catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
 });
 
 
