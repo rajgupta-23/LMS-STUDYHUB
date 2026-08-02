5bf84f8697a3cbfccb4cf0cee4f050cb7fdb4cdb
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 23:19:44 +0530

    Update qrGenerator.js

--- server/utils/qrGenerator.js
+++ server/utils/qrGenerator.js
@@ -1,30 +1,17 @@
 const QRCode = require("qrcode");
 
-
 const generateQRCode = async (certificateId) => {
-
     try {
-
         const verificationUrl =
             `${process.env.CLIENT_URL}/verify/${certificateId}`;
-
-
         const qrCode =
             await QRCode.toDataURL(verificationUrl);
-
-
         return qrCode;
-
-
     } catch (error) {
-
         throw new Error(
             "Failed to generate QR Code"
         );
-
     }
-
 };
-
 
 module.exports = generateQRCode;
