2264f8ad880d6bffddba188cc5754c6e84c8b7d2
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 23:17:03 +0530

    Update paymentRoutes.js

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,34 +1,14 @@
 const express = require("express");
-
 const router = express.Router();
-
 const { protect } = require("../middleware/auth");
-
 const {
     createOrder,
     verifyPayment,
     paymentHistory
-
 } = require("../controllers/paymentController");
-router.post(
-    "/create-order",
-    protect,
-    createOrder
-);
 
 
-
-router.post(
-    "/verify",
-    protect,
-    verifyPayment
-);
-
-
-
-router.get(
-    "/history",
-    protect,
-    paymentHistory
-);
+router.post("/create-order",protect,createOrder);
+router.post("/verify", protect,verifyPayment);
+router.get("/history",protect,paymentHistory);
 module.exports = router;
