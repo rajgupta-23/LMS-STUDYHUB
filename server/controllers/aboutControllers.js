6674e92701c9ddcb30ffd52d0e95a211c8b0bf9f
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 22:45:45 +0530

    Update aboutControllers.js

--- server/controllers/aboutControllers.js
+++ server/controllers/aboutControllers.js
@@ -3,28 +3,19 @@ const User = require("../models/User");
 const Review = require("../models/Review");
 
 
+
 const getAboutStats = async (req, res) => {
-
     try {
-
-
         const courses =
             await Course.countDocuments();
-
-
         const students =
             await User.countDocuments({
                 role: "student"
             });
-
-
         const instructors =
             await User.countDocuments({
                 role: "instructor"
             });
-
-
-
         const ratingData =
             await Review.aggregate([
 
@@ -39,40 +30,25 @@ const getAboutStats = async (req, res) => {
 
             ]);
 
-
-
         const rating =
             ratingData.length > 0
                 ?
                 ratingData[0].avgRating.toFixed(1)
                 :
                 "0";
-
-
-
         res.json({
-
             courses,
             students,
             instructors,
             rating
-
         });
-
-
     }
     catch (error) {
-
         res.status(500).json({
             message: error.message
         });
-
     }
-
-
 };
-
-
 module.exports = {
     getAboutStats
 };
