c96b5477e65169f8dbcf52f63568a72d784fdef2
Rajat Yadav <ry0984231@gmail.com>
2026-08-02 23:20:01 +0530

    Update uploadCertificate.js

--- server/utils/uploadCertificate.js
+++ server/utils/uploadCertificate.js
@@ -4,11 +4,10 @@ const uploadCertificate = async (filePath, certificateId) => {
     try {
         const result = await cloudinary.uploader.upload(filePath, {
             folder: "lms-certificates",
-            resource_type: "raw", // PDF upload
+            resource_type: "raw", 
             public_id: certificateId,
             overwrite: true,
         });
-
         return result.secure_url;
     } catch (error) {
         throw new Error("Certificate upload failed: " + error.message);
